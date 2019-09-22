dd1ac6f1e32c1189210fa327ead95f9f6ddd6e63
You Song Hou <yousonghou@yahoo.com>
2019-09-22 00:08:01 -0400

    add home buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div className="home__background">
           <div className="home__container">
             <h2 className="white-text">Welcome to Help Farm</h2>
+            <Link to="/groups" className="home__button">Discover</Link>
+            <Link to="/createGroup" className="home__button">Create Group</Link>
           </div>
         </div>
     );
