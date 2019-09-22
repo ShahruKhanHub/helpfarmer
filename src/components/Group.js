3b94ecc6a30677630bcb644b1d2b529f91cb6fbf
You Song Hou <yousonghou@yahoo.com>
2019-09-22 09:24:01 -0400

    add logo

--- src/components/Group.js
+++ src/components/Group.js
@@ -5,55 +5,55 @@ class Group extends Component{
         return(
             <div className="row">
                 <h2>Group A</h2>
-                <div className="col s12 m12 l6">
+                <div className="col s12 m12 l5">
                     <h3>Members</h3>
-                    <div className="card amber darken-3">
+                    <div className="card grey darken-3">
                         <div className="card-content white-text">
-                        <span className="card-title">Member 1</span>
+                        <span className="card-title">Surya Ram</span>
                         </div>
                     </div>
-                    <div className="card amber darken-3">
+                    <div className="card grey darken-3">
                         <div className="card-content white-text">
-                        <span className="card-title">Member 1</span>
+                        <span className="card-title">Siva Mani</span>
                         </div>
                     </div>
-                    <div className="card amber darken-3">
+                    <div className="card grey darken-3">
                         <div className="card-content white-text">
-                        <span className="card-title">Member 1</span>
+                        <span className="card-title">Suri Ravi</span>
                         </div>
                     </div>
                 </div>
-                <div className="col s12 m12 l6">
+                <div className="col s12 m12 l5">
                     <h3>Proposals</h3>
-                    <div className="card cyan lighten-3">
+                    <div className="card grey darken-3">
                         <div className="card-content white-text">
                         <div className="card__flex">
                             <span className="card-title">Proposal 1</span>
                             <div>
-                                <p className="card__button">Yes</p>
-                                <p className="card__button">No</p>
+                                <button className="card__button">Yes</button>
+                                <button className="card__button">No</button>
                             </div>
                         </div>
                         </div>
                     </div>
-                    <div className="card cyan lighten-3">
+                    <div className="card grey darken-3">
                         <div className="card-content white-text">
                         <div className="card__flex">
                             <span className="card-title">Proposal 2</span>
                             <div>
-                                <p className="card__button">Yes</p>
-                                <p className="card__button">No</p>
+                                <buttton className="card__button">Yes</buttton>
+                                <buttton className="card__button">No</buttton>
                             </div>
                         </div>
                         </div>
                     </div>
-                    <div className="card cyan lighten-3">
+                    <div className="card grey darken-3">
                         <div className="card-content white-text">
                         <div className="card__flex">
                             <span className="card-title">Proposal 3</span>
                             <div>
-                                <p className="card__button">Yes</p>
-                                <p className="card__button">No</p>
+                                <button className="card__button">Yes</button>
+                                <button className="card__button">No</button>
                             </div>
                         </div>
                         </div>
--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../../img/logo.png';
+
 const Nav = () => {
     return(
         <div>
             <nav>
                 <div className="nav-wrapper">
-                <Link to="/" className="brand-logo">Logo</Link>
+                <Link to="/" className="brand-logo">
+                    <img src={Logo} className="logo__image" alt="Logo"></img>
+                </Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/groups">Groups</Link></li>
