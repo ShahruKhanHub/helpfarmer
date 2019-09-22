415590496a7b2cd53ecb75684d02aed4e3754803
You Song Hou <yousonghou@yahoo.com>
2019-09-22 10:11:47 -0400

    add login button

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,12 @@ const Home = () => {
         <div className="home__background">
           <div className="home__container">
             <h2 className="white-text marginBottom">Welcome to Farm Unity</h2>
+            <br />
             <Link to="/groups" className="home__button">Discover</Link>
             <Link to="/createGroup" className="home__button">Create Group</Link>
+            <Link to="/createGroup" className="login__button">
+              <p>Login</p>
+            </Link>
           </div>
         </div>
     );
